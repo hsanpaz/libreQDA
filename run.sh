5dc24dd723074a888462f6343cffa55a6cb36c6e
Lu <lupa@inventati.org>
2017-03-14 14:21:50 -0300

    trying init works

--- run.sh
+++ run.sh
@@ -8,8 +8,6 @@ then
 else
   echo "Primer corrida"
   sleep 20 #esperando q levate bbdd
-  python manage.py syncdb --noinput
-  echo "from django.contrib.auth.models import User; User.objects.create_superuser('noresponder@libreqda.edu.uy', 'admin', 'admin')" | python manage.py shell
-  touch ./$FILE
+  python manage.py syncdb --noinput &&  touch ./$FILE
 fi
 python manage.py runserver 0.0.0.0:8000
