bfb998bcbcf42c4c1e6745d9331b2a292da37c5e
Lu <lupa@inventati.org>
2017-03-14 14:31:29 -0300

    some commenst

--- run.sh
+++ run.sh
@@ -8,6 +8,8 @@ then
 else
   echo "Primer corrida"
   sleep 20 #esperando q levate bbdd
+  echo "Sincronizando"
   python manage.py syncdb --noinput &&  touch ./$FILE
 fi
+echo "Levantando el server"
 python manage.py runserver 0.0.0.0:8000
